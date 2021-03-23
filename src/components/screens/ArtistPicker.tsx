d84543b0432bca44243007d2b32d2c4c400e16e4
flaaaps <julianwening4@gmail.com>
2021-03-23 23:44:20 +0100

    :truck: Move ArtistPicker component into screens directory

--- src/components/screens/ArtistPicker.tsx
+++ src/components/screens/ArtistPicker.tsx
@@ -1,33 +1,34 @@
 import classNames from "classnames"
 import { motion } from "framer-motion"
 import React, { ChangeEvent, useContext, useEffect, useState } from "react"
-import { getTopArtists, searchArtists } from "../api/spotify"
-import { Artist } from "../types/spotify"
-import { UserContext } from "./App"
-import ArtistCard from "./artist/Card"
-import ArtistSearch from "./artist/Search"
-import ArtistSelection from "./artist/Selection"
-import { ScreenContext } from "./pages/Home"
+import { getTopArtists, searchArtists } from "../../api/spotify"
+import { Artist } from "../../types/spotify"
+import { UserContext } from "../App"
+import ArtistCard from "../artist/Card"
+import ArtistSearch from "../artist/Search"
+import ArtistSelection from "../artist/Selection"
+import { ScreenContext } from "../pages/Home"
 
 export type ArtistList = [Artist | null, Artist | null, Artist | null]
 
 type Props = {
     setSubmittedArtists: (artists: Artist[]) => void
+    submittedArtists?: Artist[]
 }
 
-const ArtistPicker: React.FC<Props> = ({ setSubmittedArtists }) => {
+const ArtistPicker: React.FC<Props> = ({ submittedArtists, setSubmittedArtists }) => {
     const [artistValue, setArtistValue] = useState("")
     const [artists, setArtists] = useState<Artist[]>([])
-    const [selectedArtists, setSelectedArtists] = useState<ArtistList>([null, null, null])
+    const [selectedArtists, setSelectedArtists] = useState<ArtistList>(
+        submittedArtists ? (submittedArtists as ArtistList) : [null, null, null]
+    )
 
     const { setCurrentScreen } = useContext(ScreenContext)
-
     const { accessToken } = useContext(UserContext)
-
     const isFull = selectedArtists.indexOf(null) === -1
 
     useEffect(() => {
-        searchArtists(artistValue, accessToken!!).then(newArtists => {
+        searchArtists(artistValue).then(newArtists => {
             const artistsToPush: Artist[] = []
             if (newArtists.length > 0) {
                 newArtists.forEach(art => {
@@ -36,7 +37,7 @@ const ArtistPicker: React.FC<Props> = ({ setSubmittedArtists }) => {
                 setArtists(artistsToPush)
             }
         })
-    }, [artistValue, accessToken])
+    }, [artistValue])
 
     useEffect(() => {
         setTopArtists()
@@ -44,7 +45,7 @@ const ArtistPicker: React.FC<Props> = ({ setSubmittedArtists }) => {
     }, [accessToken])
 
     const setTopArtists = () => {
-        getTopArtists(accessToken!!).then(topArtists => {
+        getTopArtists().then(topArtists => {
             setArtists(topArtists)
         })
     }
@@ -55,7 +56,6 @@ const ArtistPicker: React.FC<Props> = ({ setSubmittedArtists }) => {
     }
 
     const setSelectedArtist = (artist: Artist) => {
-        console.log("Set top artist:", artist)
         setSelectedArtists(prevSelected => {
             const newSelected: ArtistList = [prevSelected[0], prevSelected[1], prevSelected[2]]
             newSelected[prevSelected.indexOf(null)] = artist
@@ -67,9 +67,7 @@ const ArtistPicker: React.FC<Props> = ({ setSubmittedArtists }) => {
         setSelectedArtists(prevSelected => {
             const newSelected: ArtistList = [...prevSelected]
             const found = prevSelected.find(prev => prev?.id === artistId)
-            console.log(found)
             if (found) newSelected[prevSelected.indexOf(found)] = null
-            console.log(newSelected)
             return newSelected
         })
     }
